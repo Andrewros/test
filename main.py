f9fcc79460a1d9452ff034ce3a076f3862fd056f
Andrew <97545903+Andrewros@users.noreply.github.com>
2023-05-29 17:24:23 -0400

    test

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+loggedin=False
+app=Flask(__name__)
+def bolden(f):
+    def inner():
+        if loggedin:
+            return f"<h1>{f()}</h1>"
+        else:
+            return "Please wait a bit before doing this"
+    return inner
+@app.route("/")
+@bolden
+def main():
+    return "Hello World"
+if __name__=="__main__":
+    app.run(port=3000, debug=True)
